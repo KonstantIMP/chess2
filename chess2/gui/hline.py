7925449c6ce9bb0b63434a995f61b7c5d324b7da
Sashagran <113104217+Sashagran@users.noreply.github.com>
2022-12-13 14:55:21 +0300

    comments

--- chess2/gui/hline.py
+++ chess2/gui/hline.py
@@ -1,5 +1,6 @@
 """
 Горизонтальная линия(отделитель)
+Виджет, элемент интерфейса
 """
 from PyQt6.QtWidgets import QFrame
 
@@ -7,10 +8,15 @@ from PyQt6.QtWidgets import QFrame
 class HLine(QFrame):
     """
     Черная горизонтальная полоса
+    QFrame модифицируем в линию
     """
     def __init__(self):
         super(HLine, self).__init__()
-
+        """
+            последние две строки
+делают из квадрата
+горизонтальную линию
+            """
         self.setFrameShape(QFrame.Shape.HLine)
         self.setFrameShadow(QFrame.Shadow.Sunken)
 
